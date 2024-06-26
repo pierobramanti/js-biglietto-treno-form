6b388c51feccd5cc80b92ce65bdec73041538ce7
pierobramanti <piero.bramanti94@gmail.com>
2024-06-26 19:43:31 +0200

    [UPD] inserted addEventListener on button and declared variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-// INSERISCO VARIABILE BTN e ASSEGNO VALORE DOCUMENT 
+// INSERISCO VARIABILE BTN e ASSEGNO VALORE DOCUMENT //
 const btn = document.getElementById("button")
+
+// APRO FUNZIONE E INSERISCO VARIABILI //
+btn.addEventListener("click", function () {
+    
+    let name_surname= document.getElementById("nomeEcognome").value;
+    let km = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
+}
